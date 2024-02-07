c55f696627737a7d9072e407369542fadda85143
ELIJAHH <144183598+eliiiii-lam@users.noreply.github.com>
2024-02-06 17:51:42 -0800

    Add files via upload

---
+++ Vision.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.AprilTag;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@Autonomous
+public class Vision extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder(){
+            
+        }
+    }
+}
